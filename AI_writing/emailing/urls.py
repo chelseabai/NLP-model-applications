ca3e3d18aabb0f1ad80044e03e314e3d025d99cf
Xinyu Bai <53417086+chelseabai@users.noreply.github.com>
2021-07-22 02:31:20 +0100

    Delete AI_writing directory

--- AI_writing/emailing/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("email", views.email, name="email"),
-    path("excel", views.excel, name="excel"),
-    path("advertise", views.advertise, name="advertise"),
-    path("", views.gptmodel, name="model")]
